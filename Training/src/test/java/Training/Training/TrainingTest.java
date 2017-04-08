308d265462ac05537d0356c4f7d94f29dde79ada
fernando.f.andrade <fernando.f.andrade@192.168.0.24>
2017-04-08 09:58:09 -0300

    08/04 9:58

--- Training/src/test/java/Training/Training/TrainingTest.java
+++ Training/src/test/java/Training/Training/TrainingTest.java
@@ -42,7 +42,7 @@ public class TrainingTest {
 		driverAndroid = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidade);
 		
 
-		// testing devops jenkis server
+		// testing devops jenkis server 8/04/2017
 	}
 
 	@Test
